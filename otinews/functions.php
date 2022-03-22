66314a887b038eb41710050ff0f5c72fae9c7a3f
BabboNataleJr <83788744+BabboNataleJr@users.noreply.github.com>
2022-03-22 12:35:26 +0100

    Real First Commit

--- otinews/functions.php
+++ otinews/functions.php
@@ -1,8 +1,18 @@
 <?php
 
-function otinews_load_script(){
+    function otinews_load_scripts()
+    {
+        $version = wp_get_theme()->wp_get_option("Version");
+        // Theme Main Style
+        wp_enqueue_style('theme_style', get_template_directory_uri() . 'style.css', array(), $version, 'all');
+    }
 
-    $version = wp_get_theme()-> wp_get_option('Version');
-    wp_enqueue_style( '', '', array(), $version, '' )
+    add_action('wp_enqueue_scripts', 'otinews_load_scripts');
 
-}
+    function otinews_menu_config()
+    {
+        register_nav_menus( array(
+            'otinews_header_menu'      => 'otinews Header Menu',
+            'otinews_footer_menu'      => 'otinews Footer Menu'
+        ) )
+    }
