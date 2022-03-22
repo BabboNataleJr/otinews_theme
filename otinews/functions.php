0dc517b7d09648ca2bf46915b8e6ff26b727fe36
BabboNataleJr <83788744+BabboNataleJr@users.noreply.github.com>
2022-03-22 12:48:10 +0100

    corrected

--- otinews/functions.php
+++ otinews/functions.php
@@ -2,9 +2,9 @@
 
     function otinews_load_scripts()
     {
-        $version = wp_get_theme()->wp_get_option("Version");
+        $version = wp_get_theme()->get( 'Version' );
         // Theme Main Style
-        wp_enqueue_style('theme_style', get_template_directory_uri() . 'style.css', array(), $version, 'all');
+        wp_enqueue_style('otinews-theme-style', get_template_directory_uri() . '/style.css', array(), $version, 'all');
     }
 
     add_action('wp_enqueue_scripts', 'otinews_load_scripts');
@@ -12,7 +12,7 @@
     function otinews_menu_config()
     {
         register_nav_menus( array(
-            'otinews_header_menu'      => 'otinews Header Menu',
-            'otinews_footer_menu'      => 'otinews Footer Menu'
+            'otinews-header-menu'      => 'Otinews Header Menu',
+            'otinews-footer-menu'      => 'Otinews Footer Menu'
         ) )
     }
