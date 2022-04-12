59f047360e600795d578d66d6cb52eb671e51989
BabboNataleJr <83788744+BabboNataleJr@users.noreply.github.com>
2022-04-12 16:12:44 +0200

    add some style

---
+++ otinews/searchform.php
@@ -0,0 +1,11 @@
+<form action="<?php echo esc_url( home_url('/') );?>" method="get" role="search" class="topbar__form">
+    <label for="s" class="screen-reader-text">
+        <?php echo _x('Search for:', 'label', 'otinews');?>
+    </label>
+    <div class="search__form__content">
+        <i class="fa fa-search"></i>
+        <input name="s" type="text" class="form-control form-input search-field topbar__search__input"
+            placeholder="<?php echo esc_attr_x('What are we searching?', 'placeholder', 'otinews');?>"
+            value="<?php echo get_search_query();?>">
+    </div>
+</form>
